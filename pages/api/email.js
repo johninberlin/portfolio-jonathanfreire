bb2d84b2a6fa0b9d5fcb918cf891ad789374a1de
Jonathan Freire Espinoza <jonathan.freireespinoza@gmail.com>
2022-01-28 16:53:54 +0100

    Edit email.js to show the error with oauth2Client.getAccessToken

--- pages/api/email.js
+++ pages/api/email.js
@@ -24,7 +24,7 @@ export default async function email(req, res) {
     const accessToken = await new Promise((resolve, reject) => {
       oauth2Client.getAccessToken((err, token) => {
         if (err) {
-          reject("Failed to create access token :(");
+          reject("Failed to create access token, " + err);
         }
         resolve(token);
       });
