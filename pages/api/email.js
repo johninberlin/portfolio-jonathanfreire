0d1d649ffb92888c6bead3f9e98ffe08d777c455
Jonathan Freire Espinoza <jonathan.freireespinoza@gmail.com>
2022-01-28 16:19:06 +0100

    Edit api/email.js with an async function to fix error:No refresh token or refresh handler callback is set.

--- pages/api/email.js
+++ pages/api/email.js
@@ -11,28 +11,38 @@ export default async function email(req, res) {
     SENDER_EMAIL_ADDRESS, //gmail account used to set the project up in google cloud console"
   } = process.env;
 
-  const oauth2Client = new OAuth2(
-    MAILING_SERVICE_CLIENT_ID,
-    MAILING_SERVICE_CLIENT_SECRET,
-    OAUTH_PLAYGROUND
-  );
-  oauth2Client.setCredentials({
-    refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
-  });
-  //accessToken will retrieve a new access token when it expires
-  const accessToken = oauth2Client.getAccessToken();
+  const createTransporter = async () => {
+    const oauth2Client = new OAuth2(
+      MAILING_SERVICE_CLIENT_ID,
+      MAILING_SERVICE_CLIENT_SECRET,
+      OAUTH_PLAYGROUND
+    );
+    oauth2Client.setCredentials({
+      refresh_token: MAILING_SERVICE_REFRESH_TOKEN,
+    });
+    //accessToken will retrieve a new access token when it expires
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          reject("Failed to create access token :(");
+        }
+        resolve(token);
+      });
+    });
 
-  const smtpTransport = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      type: 'OAuth2',
-      user: SENDER_EMAIL_ADDRESS,
-      clientId: MAILING_SERVICE_CLIENT_ID,
-      clientSecret: MAILING_SERVICE_CLIENT_SECRET,
-      refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
-      accessToken,
-    },
-  });
+    const smtpTransport = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: SENDER_EMAIL_ADDRESS,
+        clientId: MAILING_SERVICE_CLIENT_ID,
+        clientSecret: MAILING_SERVICE_CLIENT_SECRET,
+        refreshToken: MAILING_SERVICE_REFRESH_TOKEN,
+        accessToken,
+      },
+    });
+    return smtpTransport;
+  }
 
   const {name, email, message} = req.body;
   const mailData = {
@@ -42,13 +52,22 @@ export default async function email(req, res) {
     text: message,
     html: `<div><p>${message}</p></div>`
   }
-  smtpTransport.sendMail(mailData, function (err, info) {
-    if(err){
-      console.log(err);
-    }else{
-      console.log("sended");
-      smtpTransport.close();
+
+  const sendEmail = async (emailOptions) => {
+    try{
+      let emailTransporter = await createTransporter();
+      await emailTransporter.sendMail(emailOptions, function (err, info) {
+        if(err){
+          console.log(err);
+        }else{
+          console.log(info);
+        }
+        emailTransporter.close();
+      })
+    }catch(e){
+      console.log(e);
     }
-  })
+  };
+  sendEmail(mailData);
   res.status(200).json({});
 }
